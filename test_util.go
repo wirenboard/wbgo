6b2af2f5db66d36f7e3fc761d852c48b7fc2b844
Ivan Shvedunov <ivan4th@gmail.com>
2015-02-19 12:46:28 +0300

    Added WaitFor() test utility.

--- test_util.go
+++ test_util.go
@@ -2,9 +2,21 @@ package wbgo
 
 import (
 	"fmt"
+	"time"
 	"testing"
 	"github.com/stretchr/testify/assert"
 )
+
+const (
+	WAIT_INTERVAL_MS = 10
+	WAIT_COUNT = 300
+)
+
+func WaitFor(t *testing.T, pred func() bool) {
+	for n := 0; !pred() && n < WAIT_COUNT; n++ {
+		time.Sleep(WAIT_INTERVAL_MS * time.Millisecond)
+	}
+}
 
 type Recorder struct {
 	t *testing.T
