6e2d1c3d8456321fe0855bf6e8237ea960db4c2c
Ivan Shvedunov <ivan4th@gmail.com>
2015-03-30 12:01:11 +0300

    Added syslog support.

--- log.go
+++ log.go
@@ -2,8 +2,10 @@ package wbgo
 
 import (
 	"os"
+	"path"
 	"io/ioutil"
 	"log"
+	"log/syslog"
 )
 
 var (
@@ -11,6 +13,8 @@ var (
 	Warn *log.Logger
 	Info  *log.Logger
 	Debug *log.Logger
+	debuggingEnabled bool = false
+	useSyslog = false
 )
 
 func init() {
@@ -20,10 +24,34 @@ func init() {
 	Debug = log.New(ioutil.Discard, "", 0)
 }
 
+func makeSyslogger(priority syslog.Priority, prefix string) *log.Logger {
+	writer, err := syslog.New(syslog.LOG_DAEMON | syslog.LOG_INFO, path.Base(os.Args[0]))
+	if err != nil {
+		log.Panicf("syslog init failed: %s", err)
+	}
+	return log.New(writer, prefix, 0)
+}
+
 func SetDebuggingEnabled(enable bool) {
-	if (enable) {
-		Debug = log.New(os.Stderr, "DEBUG: ", log.LstdFlags)
-	} else {
+	debuggingEnabled = enable
+	updateDebugLogger()
+}
+
+func updateDebugLogger() {
+	switch {
+	case !debuggingEnabled:
 		Debug = log.New(ioutil.Discard, "", 0)
-	}	
+	case useSyslog:
+		Debug = makeSyslogger(syslog.LOG_DAEMON | syslog.LOG_DEBUG, "DEBUG: ")
+	default:
+		Debug = log.New(os.Stderr, "DEBUG: ", log.LstdFlags)
+	}
+}
+
+func UseSyslog() {
+	useSyslog = true
+	Error = makeSyslogger(syslog.LOG_DAEMON | syslog.LOG_ERR, "ERROR: ")
+	Warn = makeSyslogger(syslog.LOG_DAEMON | syslog.LOG_WARNING, "WARNING: ")
+	Info = makeSyslogger(syslog.LOG_DAEMON | syslog.LOG_INFO, "INFO: ")
+	updateDebugLogger()
 }
